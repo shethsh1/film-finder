c51ce2a186615e657af3bb072e1f8d2f91f52ba6
shaahid <shaahidsheth1234@hotmail.com>
2023-03-04 00:32:10 -0500

    created animeSlice

---
+++ src/features/animeSlice.ts
@@ -0,0 +1,124 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getPopularAnime: any = createAsyncThunk(
+  "popularAnime/getPopularAnime",
+  async (page) => {
+    const response = await fetch(
+      `https://api.jikan.moe/v4/top/anime?page=${page}`
+    );
+    const formatResponse = await response.json();
+    return formatResponse;
+  }
+);
+
+export const getUpcomingAnime: any = createAsyncThunk(
+  "animeSeasons/getAnimeRecommendations",
+  async (page) => {
+    const response = await fetch(
+      `https://api.jikan.moe/v4/seasons/upcoming?page=${page}`
+    );
+    const formatResponse = await response.json();
+    return formatResponse;
+  }
+);
+
+export const getAnimeDetails: any = createAsyncThunk(
+  "animeDetails/getAnimeDetails",
+  async (mal_id) => {
+    const response = await fetch(`https://api.jikan.moe/v4/anime/${mal_id}`);
+    const formatResponse = await response.json();
+    return formatResponse;
+  }
+);
+
+interface AnimeObject {
+  mal_id: number;
+  images: {
+    jpg?: {
+      image_url: string;
+    };
+    webp?: {
+      image_url: string;
+    };
+  };
+  title: string;
+  status: string;
+  score: string;
+  genres: { mal_id: number; name: string }[];
+}
+
+interface interfaceAnimeDetail {
+  mal_id: number;
+  images: {
+    jpg?: {
+      image_url: string;
+    };
+    webp?: {
+      image_url: string;
+    };
+  };
+  trailer: {
+    youtube_id: string;
+  };
+  title: string;
+  status: string;
+  genres: { mal_id: number; name: string }[];
+}
+
+interface AnimeState {
+  activeAnimes: {
+    data: AnimeObject;
+    pagination: {
+      current_page: number;
+    };
+  }[];
+  loading: boolean;
+  cardDetailLoading: boolean;
+  animeDetail: interfaceAnimeDetail | null;
+}
+
+export const animeSlice = createSlice({
+  name: "anime",
+  initialState: {
+    activeAnimes: [],
+    loading: false,
+    cardDetailLoading: false,
+    animeDetail: null,
+  } as AnimeState,
+  reducers: {},
+  extraReducers: {
+    [getPopularAnime.pending]: (state) => {
+      state.loading = true;
+    },
+    [getPopularAnime.fulfilled]: (state, action) => {
+      state.activeAnimes = action.payload;
+      state.loading = false;
+    },
+    [getPopularAnime.rejected]: (state) => {
+      state.loading = false;
+    },
+    [getUpcomingAnime.pending]: (state) => {
+      state.loading = true;
+    },
+    [getUpcomingAnime.fulfilled]: (state, action) => {
+      state.activeAnimes = action.payload;
+      state.loading = false;
+    },
+    [getUpcomingAnime.rejected]: (state) => {
+      state.loading = false;
+    },
+
+    [getAnimeDetails.pending]: (state) => {
+      state.cardDetailLoading = true;
+    },
+    [getAnimeDetails.fulfilled]: (state, action) => {
+      state.activeAnimes = action.payload;
+      state.cardDetailLoading = false;
+    },
+    [getAnimeDetails.rejected]: (state) => {
+      state.cardDetailLoading = false;
+    },
+  },
+});
+
+export default animeSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,21 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
-import MovieReducer from '../features/movieSlice'
-import ShowReducer from '../features/showSlice'
+import MovieReducer from "../features/movieSlice";
+import ShowReducer from "../features/showSlice";
+import AnimeReducer from "../features/animeSlice";
 import { moviesApi, showsApi } from "../features/apiSlice";
 
 export const store = configureStore({
   reducer: {
     movie: MovieReducer,
     show: ShowReducer,
+    anime: AnimeReducer,
     [moviesApi.reducerPath]: moviesApi.reducer,
-    [showsApi.reducerPath]: showsApi.reducer
+    [showsApi.reducerPath]: showsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-  getDefaultMiddleware().concat(moviesApi.middleware, showsApi.middleware),
+    getDefaultMiddleware().concat(moviesApi.middleware, showsApi.middleware),
   devTools: true,
-})
-
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
