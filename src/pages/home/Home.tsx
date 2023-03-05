bcc62b30be07418f07a056ba9b39c6f8e76c3481
shaahid <shaahidsheth1234@hotmail.com>
2023-03-05 17:55:37 -0500

    updated

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,27 @@
 import { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "../../context/Theme/ThemeContext";
 import classNames from "classnames";
-import { XLContainer, MDContainer } from "../../components";
+import { XLContainer, MDContainer, Collapse } from "../../components";
 
 const movies = [
+  {
+    label: "test",
+  },
+  {
+    label: "test2",
+  },
+  {
+    label: "test3",
+  },
+  {
+    label: "test4",
+  },
+  {
+    label: "test5",
+  },
+  {
+    label: "test6",
+  },
   {
     label: "test",
   },
@@ -27,11 +45,23 @@ const movies = [
 type ActiveTab = "Movies" | "Shows" | "Anime";
 
 export const Home = () => {
+  let timeout: string | number | NodeJS.Timeout | undefined;
   const { theme } = useContext(ThemeContext);
   const isDarkMode = theme === "dark" ? true : false;
   const [activeTab, setActiveTab] = useState<ActiveTab>("Movies");
-
   const [searchTerm, setSearchTerm] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleMouseLeave = () => {
+    timeout = setTimeout(() => {
+      setIsFocused(false);
+    }, 600); // Set the delay time in milliseconds
+  };
+
+  const handleMouseEnter = () => {
+    clearTimeout(timeout);
+    setIsFocused(true);
+  };
 
   function handleInputChange(event: any) {
     setSearchTerm(event.target.value);
@@ -50,6 +80,8 @@ export const Home = () => {
     <div className="mt-16">
       <MDContainer>
         <div
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
           className={classNames({
             "text-dark-font-primary": isDarkMode,
             "text-light-font-primary": !isDarkMode,
@@ -74,63 +106,67 @@ export const Home = () => {
             {/* Search below starts */}
             <div
               className={classNames(
-                "absolute z-10 w-full cursor-pointer select-none mt-2 origin-top-right divide-y divide-gray-100 rounded-md shadow-lg ring-black ring-opacity-5 focus:outline-none",
+                "absolute z-10 w-full cursor-pointer select-none mt-2 origin-top-right divide-gray-100 rounded-md shadow-lg ring-black ring-opacity-5 focus:outline-none",
                 {
                   "bg-light-secondary": !isDarkMode,
                   "bg-dark-secondary": isDarkMode,
                 }
               )}
             >
-              <div className="p-4">
-                {/* tabs start */}
-                <div id="tabs" className="inline-flex font-bold gap-4">
-                  <button
-                    disabled={activeTab === "Movies"}
-                    onClick={() => handleActiveTab("Movies")}
-                    className={classNames({
-                      "border-light-tertiary border-b-2":
-                        !isDarkMode && activeTab === "Movies",
-                      "border-dark-tertiary border-b-2":
-                        isDarkMode && activeTab === "Movies",
-                    })}
-                  >
-                    Movies
-                  </button>
-                  <button
-                    disabled={activeTab === "Shows"}
-                    onClick={() => handleActiveTab("Shows")}
-                    className={classNames({
-                      "border-light-tertiary border-b-2":
-                        !isDarkMode && activeTab === "Shows",
-                      "border-dark-tertiary border-b-2":
-                        isDarkMode && activeTab === "Shows",
-                    })}
-                  >
-                    Shows
-                  </button>
-                  <button
-                    disabled={activeTab === "Anime"}
-                    onClick={() => handleActiveTab("Anime")}
-                    className={classNames({
-                      "border-light-tertiary border-b-2":
-                        !isDarkMode && activeTab === "Anime",
-                      "border-dark-tertiary border-b-2":
-                        isDarkMode && activeTab === "Anime",
-                    })}
-                  >
-                    Anime
-                  </button>
-                </div>
-                {/* tabs ends */}
-
-                <div className="mt-4 top-ddd-border">
-                  <div className="mt-4">
-                    {movies.map((movie) => (
-                      <div>{movie.label}</div>
-                    ))}
+              <Collapse show={isFocused}>
+                <div className="p-4">
+                  {/* tabs start */}
+                  <div id="tabs" className="inline-flex font-bold gap-4">
+                    <button
+                      disabled={activeTab === "Movies"}
+                      onClick={() => handleActiveTab("Movies")}
+                      className={classNames({
+                        "border-light-tertiary border-b-2":
+                          !isDarkMode && activeTab === "Movies",
+                        "border-dark-tertiary border-b-2":
+                          isDarkMode && activeTab === "Movies",
+                      })}
+                    >
+                      Movies
+                    </button>
+                    <button
+                      disabled={activeTab === "Shows"}
+                      onClick={() => handleActiveTab("Shows")}
+                      className={classNames({
+                        "border-light-tertiary border-b-2":
+                          !isDarkMode && activeTab === "Shows",
+                        "border-dark-tertiary border-b-2":
+                          isDarkMode && activeTab === "Shows",
+                      })}
+                    >
+                      Shows
+                    </button>
+                    <button
+                      disabled={activeTab === "Anime"}
+                      onClick={() => handleActiveTab("Anime")}
+                      className={classNames({
+                        "border-light-tertiary border-b-2":
+                          !isDarkMode && activeTab === "Anime",
+                        "border-dark-tertiary border-b-2":
+                          isDarkMode && activeTab === "Anime",
+                      })}
+                    >
+                      Anime
+                    </button>
                   </div>
+                  {/* tabs ends */}
                 </div>
-              </div>
+              </Collapse>
+
+              {/* results starts */}
+              <Collapse show={searchTerm.length > 0 && isFocused}>
+                <div className="p-4">
+                  {movies.map((movie) => (
+                    <div>{movie.label}</div>
+                  ))}
+                </div>
+              </Collapse>
+              {/* results ends */}
             </div>
             {/* Search below ends */}
           </form>
