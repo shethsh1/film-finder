ec2053bda2cea22119754cce43984825c89d8ae7
shaahid <shaahidsheth1234@hotmail.com>
2023-03-04 20:40:00 -0500

    added dropdown menu

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,31 +1,45 @@
 import classNames from "classnames";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { ThemeContext } from "../../context/Theme/ThemeContext";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const NavItems = [
+  {
+    to: "/",
+    label: "Home",
+  },
+  {
+    to: "/movies",
+    label: "Movies",
+  },
+  {
+    to: "/shows",
+    label: "Shows",
+  },
+  {
+    to: "/anime",
+    label: "Anime",
+  },
+];
+
 export const Navigation = () => {
   const { theme } = useContext(ThemeContext);
   const isDarkMode = theme === "dark" ? true : false;
 
   return (
-    <nav>
+    <nav className="mt-4">
       <ul
-        className={classNames("flex text-xl gap-8 py-4", {
+        className={classNames("flex text-xl gap-16 py-4", {
           "text-light-font-primary": !isDarkMode,
           "text-dark-font-primary": isDarkMode,
         })}
       >
-        <li>
-          <NavLink to="/test">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/movies">Movies</NavLink>
-        </li>
-        <li>
-          <NavLink to="/shows">Shows</NavLink>
-        </li>
-        <li>
-          <NavLink to="/anime">Anime</NavLink>
-        </li>
+        {NavItems.map((item, i) => (
+          <NavLink key={i} to={item.to}>
+            {item.label}
+          </NavLink>
+        ))}
       </ul>
     </nav>
   );
