ab345091890680274bb758d47bbe5026141f7da9
shaahid <shaahidsheth1234@hotmail.com>
2023-03-07 20:50:38 -0500

    try again

--- functions/moviedb.js
+++ functions/moviedb.js
@@ -4,11 +4,10 @@ exports.handler = async (event) => {
   // Get the API key from the environment variable
   const apiKey = process.env.REACT_APP_MOVIE_DB_KEY;
   // Get the page number from the query string
-  const page = event.queryStringParameters.page || 1;
   console.log(event);
   // Get the endpoint from the path parameter
   // Construct the API URL with the key, page and endpoint
-  const apiUrl = `https://api.themoviedb.org/3/${event.path}?api_key=${apiKey}&language=en-US&page=${page}`;
+  const apiUrl = `https://api.themoviedb.org/3/${event.path}?api_key=${apiKey}&${event.queryStringParameters}`;
   try {
     const response = await axios.get(apiUrl);
     return {
