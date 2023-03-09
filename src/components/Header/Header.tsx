a2d241b096daae09c82448503db69fa65d51e697
shaahid <shaahidsheth1234@hotmail.com>
2023-03-08 23:04:49 -0500

    updated nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,35 @@
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../../images/logo192.png";
 import { ThemeChangeButton } from "../ThemeChangeButton/ThemeChangeButton";
+import { Navigation } from "../Navigation/Navigation";
+import { ThemeContext } from "../../context/Theme/ThemeContext";
+import classNames from "classnames";
 
 export const Header = () => {
+  const { theme } = useContext(ThemeContext);
+  const isDarkMode = theme === "dark" ? true : false;
   return (
-    <header className="flex justify-between items-center">
-      <div>
-        <NavLink to="/">
-          <img src={logo} alt="logo" className="w-8 h-8" />
+    <div
+      className={classNames("h-[128px] fixed z-50 w-full px-6 pt-3", {
+        "bg-light-primary text-light-font-primary": !isDarkMode,
+        "bg-dark-primary text-dark-font-primary": isDarkMode,
+      })}
+    >
+      <header className="flex justify-between items-center">
+        <NavLink to="/" className="flex gap-4 items-center">
+          <div>
+            <img src={logo} alt="logo" className="w-8 h-8" />
+          </div>
+          <h1 className="text-3xl" style={{ fontFamily: "Lobster Two" }}>
+            Film Finder
+          </h1>
         </NavLink>
-      </div>
-      <div>
-        <ThemeChangeButton />
-      </div>
-    </header>
+        <div>
+          <ThemeChangeButton />
+        </div>
+      </header>
+      <Navigation />
+    </div>
   );
 };
