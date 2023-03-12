dc59b1d084f23bcc488f73f3b8783c4418ff4d93
shaahid <shaahidsheth1234@hotmail.com>
2023-03-12 15:26:33 -0400

    added sidebar skele

--- src/components/Sidebar/AnimeCards/AnimeCards.tsx
+++ src/components/Sidebar/AnimeCards/AnimeCards.tsx
@@ -4,6 +4,7 @@ import { AiFillStar } from "react-icons/ai";
 import { ThemeContext } from "../../../context/Theme/ThemeContext";
 import { Anime } from "../../../features/animeSlice";
 import { format } from "date-fns";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 interface Props {
   topAnime: Anime;
@@ -16,37 +17,54 @@ export const AnimeCards: React.FC<Props> = ({ topAnime, goToMedia }) => {
 
   return (
     <div className="flex flex-col gap-4">
-      {topAnime?.data?.slice(0, 10).map((anime) => (
-        <div
-          key={anime.mal_id}
-          onClick={() => goToMedia(anime.mal_id)}
-          className={classNames("flex text-xs gap-4 cursor-pointer", {
-            "text-dark-font-primary bg-dark-secondary hover:bg-dark-hover":
-              isDarkMode,
-            "text-light-font-primary bg-light-secondary hover:bg-light-hover":
-              !isDarkMode,
-          })}
-        >
-          <img
-            className="h-20 w-16 flex-shrink-0"
-            src={anime.images.jpg?.image_url}
-            alt={anime.title}
-          />
-          <div className="py-2 mt-2">
-            <p className="font-bold">{anime.title}</p>
-            <div className="inline-flex gap-1 mt-1 items-center">
-              <div className="inline-flex text-md items-center gap-1">
-                <AiFillStar color="gold" size={16} />
-                <span>{anime.score}</span>
+      {topAnime?.data ? (
+        topAnime?.data?.slice(0, 10).map((anime) => (
+          <div
+            key={anime.mal_id}
+            onClick={() => goToMedia(anime.mal_id)}
+            className={classNames("flex text-xs gap-4 cursor-pointer", {
+              "text-dark-font-primary bg-dark-secondary hover:bg-dark-hover":
+                isDarkMode,
+              "text-light-font-primary bg-light-secondary hover:bg-light-hover":
+                !isDarkMode,
+            })}
+          >
+            <img
+              className="h-20 w-16 flex-shrink-0"
+              src={anime.images.jpg?.image_url}
+              alt={anime.title}
+            />
+            <div className="py-2 mt-2">
+              <p className="font-bold">{anime.title}</p>
+              <div className="inline-flex gap-1 mt-1 items-center">
+                <div className="inline-flex text-md items-center gap-1">
+                  <AiFillStar color="gold" size={16} />
+                  <span>{anime.score}</span>
+                </div>
+                <span>&bull;</span>
+                <span>Popularity: {anime.popularity}</span>
+                <span>&bull;</span>
+                <span>{format(new Date(anime.aired.from), "yyyy-MM-dd")}</span>
               </div>
-              <span>&bull;</span>
-              <span>Popularity: {anime.popularity}</span>
-              <span>&bull;</span>
-              <span>{format(new Date(anime.aired.from), "yyyy-MM-dd")}</span>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <SkeletonTheme
+          baseColor={isDarkMode ? "#1d2d44" : "#c7e5d0"}
+          highlightColor={isDarkMode ? "#334256" : "#b3cebb"}
+          duration={2}
+          height="75px"
+        >
+          <div className={classNames("flex flex-col gap-4 select-none")}>
+            {Array(8)
+              .fill(8)
+              .map((_, idx) => (
+                <Skeleton />
+              ))}
+          </div>
+        </SkeletonTheme>
+      )}
     </div>
   );
 };
--- src/components/Sidebar/ShowCards/ShowCards.tsx
+++ src/components/Sidebar/ShowCards/ShowCards.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import { useContext } from "react";
 import { AiFillStar } from "react-icons/ai";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import { ThemeContext } from "../../../context/Theme/ThemeContext";
 import { Shows } from "../../../features/showSlice";
 interface Props {
@@ -13,37 +14,54 @@ export const ShowCards: React.FC<Props> = ({ topShows, goToMedia }) => {
   const isDarkMode = theme === "dark" ? true : false;
   return (
     <div className="flex flex-col gap-4">
-      {topShows?.results?.slice(0, 10).map((show) => (
-        <div
-          key={show.id}
-          onClick={() => goToMedia(show.id)}
-          className={classNames("flex text-xs gap-4 cursor-pointer", {
-            "text-dark-font-primary bg-dark-secondary hover:bg-dark-hover":
-              isDarkMode,
-            "text-light-font-primary bg-light-secondary hover:bg-light-hover":
-              !isDarkMode,
-          })}
-        >
-          <img
-            className="h-20 w-16 flex-shrink-0"
-            src={`https://image.tmdb.org/t/p/w500/${show.poster_path}`}
-            alt={show.name}
-          />
-          <div className="py-2 mt-2">
-            <p className="font-bold">{show.name}</p>
-            <div className="inline-flex gap-1 mt-1 items-center">
-              <div className="inline-flex text-md items-center gap-1">
-                <AiFillStar color="gold" size={16} />
-                <span>{show.vote_average}</span>
+      {topShows?.results ? (
+        topShows?.results?.slice(0, 10).map((show) => (
+          <div
+            key={show.id}
+            onClick={() => goToMedia(show.id)}
+            className={classNames("flex text-xs gap-4 cursor-pointer", {
+              "text-dark-font-primary bg-dark-secondary hover:bg-dark-hover":
+                isDarkMode,
+              "text-light-font-primary bg-light-secondary hover:bg-light-hover":
+                !isDarkMode,
+            })}
+          >
+            <img
+              className="h-20 w-16 flex-shrink-0"
+              src={`https://image.tmdb.org/t/p/w500/${show.poster_path}`}
+              alt={show.name}
+            />
+            <div className="py-2 mt-2">
+              <p className="font-bold">{show.name}</p>
+              <div className="inline-flex gap-1 mt-1 items-center">
+                <div className="inline-flex text-md items-center gap-1">
+                  <AiFillStar color="gold" size={16} />
+                  <span>{show.vote_average}</span>
+                </div>
+                <span>&bull;</span>
+                <span>Popularity: {show.popularity}</span>
+                <span>&bull;</span>
+                <span>{show.first_air_date}</span>
               </div>
-              <span>&bull;</span>
-              <span>Popularity: {show.popularity}</span>
-              <span>&bull;</span>
-              <span>{show.first_air_date}</span>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <SkeletonTheme
+          baseColor={isDarkMode ? "#1d2d44" : "#c7e5d0"}
+          highlightColor={isDarkMode ? "#334256" : "#b3cebb"}
+          duration={2}
+          height="75px"
+        >
+          <div className={classNames("flex flex-col gap-4 select-none")}>
+            {Array(8)
+              .fill(8)
+              .map((_, idx) => (
+                <Skeleton />
+              ))}
+          </div>
+        </SkeletonTheme>
+      )}
     </div>
   );
 };
